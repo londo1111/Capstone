a959927af9024fbe7d398a98107d4c19bd9eba21
londo1111 <londonburge14@gmail.com>
2021-09-23 12:47:41 -0400

    TestUpload
    Capstone player and shooting. I am he PM please contact me if you need help.

---
+++ ReadMeNGT.txt
@@ -0,0 +1,3 @@
+Hey London here if you have any issues trying to corporate the player movement with your script please let me know.
+That also includes the shooting and bullets. If you think its something that you can fix... Try and do it on your own
+let me tell you fixing your own erorrs is a rush of dopamine lol. 
---
+++ Test 2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Animator animator;
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    Vector2 mousePos;
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
+
