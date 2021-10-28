7ec393a6a188c612998d0bd9564b11050febf4e7
joeyburke811 <joeyburke811@gmail.com>
2021-10-28 14:04:53 -0400

    Save and Load
    Partway through tutorial. Some are commented out because the player stats aren't in player script yet.

--- Test 2/Assets/ScriptiableObjects/Scripts/Player Data.cs
+++ Test 2/Assets/ScriptiableObjects/Scripts/Player Data.cs
@@ -2,21 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerData : MonoBehaviour {
+[System.Serializable]
+public class PlayerData {
   
   public int level;
   public int health;
   public float[] position;
 
-  public PlayerData (Player player)
+//Below script won't work until the player has level and health variables so it is commented out until that is done
+
+/*
+  public PlayerData (PlayerMovement player) //PlayerMovement will be the exact script name for where the health and level variables are held
   {
-      level = player.level;
-      health = player.health;
+      level = player.level;             //accessing the level in the player script
+      health = player.health;           //accessing the health in the player script
 
       position = new float[3];
       position[0] = player.transform.position.x;
       position[1] = player.transform.position.y;
       position[2] = player.transform.position.z;
   }
+  */
 
 }
