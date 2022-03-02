3f4c4ef36c3a93b571402d15e20ca3ec76101234
londo1111 <londonburge14@gmail.com>
2022-03-02 13:12:03 -0500

    Kill me

--- Test 2/Assets/Scripts/Invetory/Spawn.cs
+++ Test 2/Assets/Scripts/Invetory/Spawn.cs
@@ -14,7 +14,7 @@ public class Spawn : MonoBehaviour
 
     public void SpawnDroppedItem()
     {
-        Vector2 playerPos = new Vector2(player.position.x, player.position.y + 1);
+        Vector2 playerPos = new Vector2(player.position.x, player.position.y + 2);
         Instantiate(item, playerPos, Quaternion.identity);
     }
 }
